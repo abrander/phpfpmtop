1e93355b51d0b16fa6d4f0c7a61f337693f779c3
Anders Brander <anders@brander.dk>
2016-06-15 21:42:51 +0200

    Moved processSort to seperate file.

--- main.go
+++ main.go
@@ -74,8 +74,6 @@ type (
 		Reserved [5]byte
 	}
 
-	processSort []process
-
 	winsize struct {
 		Rows, Columns  uint16
 		XPixel, YPixel uint16
@@ -148,35 +146,6 @@ func getTerminalSize() (int, int) {
 		uintptr(unsafe.Pointer(&ws)))
 
 	return int(ws.Columns), int(ws.Rows)
-}
-
-func (s processSort) Len() int {
-	return len(s)
-}
-
-func (s processSort) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s processSort) Less(i, j int) bool {
-	a := s[i]
-	b := s[j]
-
-	// If the state are different, we sort by state.
-	if a.State != b.State {
-		// Show running processes first.
-		if a.State == "Running" {
-			return true
-		}
-		return false
-	}
-
-	// Put the slowest requests on top.
-	if a.RequestDuration > b.RequestDuration {
-		return true
-	}
-
-	return false
 }
 
 func gather(conf config, s *status) error {
---
+++ processSort.go
@@ -0,0 +1,34 @@
+package main
+
+type (
+	processSort []process
+)
+
+func (s processSort) Len() int {
+	return len(s)
+}
+
+func (s processSort) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s processSort) Less(i, j int) bool {
+	a := s[i]
+	b := s[j]
+
+	// If the state are different, we sort by state.
+	if a.State != b.State {
+		// Show running processes first.
+		if a.State == "Running" {
+			return true
+		}
+		return false
+	}
+
+	// Put the slowest requests on top.
+	if a.RequestDuration > b.RequestDuration {
+		return true
+	}
+
+	return false
+}
